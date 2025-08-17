2dbfe7756c47d27334c4c8693d5773f5e9a161b9
NguyenMinhKiet <nguyenminhkiet642002@gmail.com>
2025-08-17 23:35:13 +0700

    feat: add vercel frontend

--- dist/app.js
+++ dist/app.js
@@ -6,10 +6,12 @@ import cors from 'cors';
 import 'dotenv/config';
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:5173', // cho phép từ frontend
-    credentials: true, // nếu có dùng cookie/session
-}));
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'https://askandexplain-frontend.vercel.app/'], // cho phép từ frontend
+        credentials: true, // nếu có dùng cookie/session
+    }),
+);
 app.use('/api', Routes);
 app.use(ErrorMiddlewares);
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/askandexplain';
@@ -18,8 +20,8 @@ mongoose
     .connect(MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch((err) => {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Dừng app nếu không kết nối được DB
-});
+        console.error('MongoDB connection error:', err);
+        process.exit(1); // Dừng app nếu không kết nối được DB
+    });
 export default app;
 //# sourceMappingURL=app.js.map
